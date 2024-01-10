48cc04bb5a10445308e65be0e1520d0156bd27e4
Osama Khan <khanosama783@gmail.com>
2024-01-10 12:54:53 +0530

    Created context in infrastructure project

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace WhiteLagoon.Infrastructure.Data
 {
-    internal class ApplicationDbContext
+    internal class ApplicationDbContext : DbContext 
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
     }
 }
