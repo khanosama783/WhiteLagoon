33488c1765ba049f12f6b1fc7fb5c28c308d08f1
Osama Khan <khanosama783@gmail.com>
2024-01-10 13:37:47 +0530

    Rename column name and Add migration, update migration successfully

---
+++ WhiteLagoon.Infrastructure/Migrations/20240110080507_ModfiyNamesInVillaTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModfiyNamesInVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdateData",
+                table: "Villas",
+                newName: "Update_Data");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedData",
+                table: "Villas",
+                newName: "Created_Data");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Update_Data",
+                table: "Villas",
+                newName: "UpdateData");
+
+            migrationBuilder.RenameColumn(
+                name: "Created_Data",
+                table: "Villas",
+                newName: "CreatedData");
+        }
+    }
+}
