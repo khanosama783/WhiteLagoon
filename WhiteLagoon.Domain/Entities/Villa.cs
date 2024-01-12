21c2b5159574309f94d4e57d62f44d3d42659e73
Osama Khan <khanosama783@gmail.com>
2024-01-12 19:23:09 +0530

    add data annotations

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -10,11 +10,14 @@ namespace WhiteLagoon.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public required string Name { get; set; }
         public string? Description { get; set; }
         [Display(Name = "Price per unit")]
+        [Range(50, 500)]
         public double Price { get; set; }
         public int Sqft { get; set; }
+        [Range(1, 5)]
         public int Occupancy { get; set; }
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
