e88333b9434b36fe779a30d6888af3d315608ab0
Osama Khan <khanosama783@gmail.com>
2024-01-16 14:15:22 +0530

    Villa Number created

--- WhiteLagoon.Domain/Entities/VillaNumber.cs
+++ WhiteLagoon.Domain/Entities/VillaNumber.cs
@@ -11,10 +11,13 @@ namespace WhiteLagoon.Domain.Entities
     public class VillaNumber
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name ="Villa Number")]
         public int Villa_Number { get; set; }
         [ForeignKey("Villa")]
+        [Display(Name = "Villa Id")]
         public int VillaId { get; set; }
         public Villa Villa { get; set; }
+        [Display(Name = "Special Details")]
         public string? SpecialDetails { get; set; }
     }
 }
