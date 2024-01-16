5a67f5c6fe5c0984624e61a918f841c0b6cc08a5
Osama Khan <khanosama783@gmail.com>
2024-01-16 13:45:54 +0530

    remove seed and create villaNumber table

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -62,53 +62,6 @@ namespace WhiteLagoon.Infrastructure.Data
                     Created_Data = DateTime.Now,
                     Update_Data = DateTime.Now
                 });
-            modelBuilder.Entity<VillaNumber>().HasData(
-                new VillaNumber
-                {
-                    Villa_Number = 101,
-                    VillaId = 1,
-                },
-                new VillaNumber
-                {
-                    Villa_Number = 102,
-                    VillaId = 1,
-                },
-                new VillaNumber
-                {
-                    Villa_Number = 103,
-                    VillaId = 1,
-                },
-                new VillaNumber
-                {
-                    Villa_Number = 104,
-                    VillaId = 1,
-                },
-                new VillaNumber
-                {
-                    Villa_Number = 201,
-                    VillaId = 2,
-                },
-                new VillaNumber
-                {
-                    Villa_Number = 202,
-                    VillaId = 2,
-                },
-                new VillaNumber
-                {
-                    Villa_Number = 203,
-                    VillaId = 2,
-                },
-                new VillaNumber
-                {
-                    Villa_Number = 301,
-                    VillaId = 3,
-                },
-                 new VillaNumber
-                 {
-                     Villa_Number = 302,
-                     VillaId = 3,
-                 }
-                );
         }
     }
 }
---
+++ WhiteLagoon.Infrastructure/Migrations/20240116081245_addVillaNumber.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addVillaNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "VillaNumbers",
+                columns: table => new
+                {
+                    Villa_Number = table.Column<int>(type: "int", nullable: false),
+                    VillaId = table.Column<int>(type: "int", nullable: false),
+                    SpecialDetails = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VillaNumbers", x => x.Villa_Number);
+                    table.ForeignKey(
+                        name: "FK_VillaNumbers_Villas_VillaId",
+                        column: x => x.VillaId,
+                        principalTable: "Villas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created_Data", "Update_Data" },
+                values: new object[] { new DateTime(2024, 1, 16, 13, 42, 45, 127, DateTimeKind.Local).AddTicks(942), new DateTime(2024, 1, 16, 13, 42, 45, 127, DateTimeKind.Local).AddTicks(955) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created_Data", "Update_Data" },
+                values: new object[] { new DateTime(2024, 1, 16, 13, 42, 45, 127, DateTimeKind.Local).AddTicks(957), new DateTime(2024, 1, 16, 13, 42, 45, 127, DateTimeKind.Local).AddTicks(957) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Created_Data", "Update_Data" },
+                values: new object[] { new DateTime(2024, 1, 16, 13, 42, 45, 127, DateTimeKind.Local).AddTicks(959), new DateTime(2024, 1, 16, 13, 42, 45, 127, DateTimeKind.Local).AddTicks(960) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VillaNumbers_VillaId",
+                table: "VillaNumbers",
+                column: "VillaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VillaNumbers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created_Data", "Update_Data" },
+                values: new object[] { new DateTime(2024, 1, 10, 13, 49, 50, 397, DateTimeKind.Local).AddTicks(8195), new DateTime(2024, 1, 10, 13, 49, 50, 397, DateTimeKind.Local).AddTicks(8206) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created_Data", "Update_Data" },
+                values: new object[] { new DateTime(2024, 1, 10, 13, 49, 50, 397, DateTimeKind.Local).AddTicks(8209), new DateTime(2024, 1, 10, 13, 49, 50, 397, DateTimeKind.Local).AddTicks(8209) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Created_Data", "Update_Data" },
+                values: new object[] { new DateTime(2024, 1, 10, 13, 49, 50, 397, DateTimeKind.Local).AddTicks(8211), new DateTime(2024, 1, 10, 13, 49, 50, 397, DateTimeKind.Local).AddTicks(8211) });
+        }
+    }
+}
